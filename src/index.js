d66df0bcb845adf1d2fa8160a521b8a2269f8b5c
bjmashele <bjmashele@gmail.com>
2018-01-25 22:48:40 -0600

    added react '

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 
-console.log("My minimal React setup");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "My minimal React setup";
+
+ReactDOM.render(
+  <div>Hello React</div>,
+  document.getElementById('app')
+);
